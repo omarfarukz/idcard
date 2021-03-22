3b9345082ca047544fda036d09dfd0bee973f797
truemind9565 <omarfaruk9565@gmail.com>
2021-03-22 18:15:40 +0800

    this is first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,136 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child: Text('Demo')),
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/omar.jpg'),
+                ),
+                Text(
+                  'Omar Faruk',
+                  style: TextStyle(
+                    fontFamily: 'Redressed',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  // padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          //size: 200,
+                          color: Colors.teal.shade900,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '+60166180630',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  // padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.email,
+                          color: Colors.teal.shade900,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'omarfaruk9565@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.teal.shade900,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Saville Kajang, Sleangor , Malaysia',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
